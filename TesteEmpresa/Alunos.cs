64f86df8b11f738fc474a18abea2b93a1df031fe
Daniel Souza <danieldossantossouza.88@gmail.com>
2023-07-02 14:43:42 -0300

    subindo o teste para o  git

---
+++ TesteEmpresa/Alunos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEmpresa
+{
+    public class Alunos
+    {
+        public string nome { get; set; }
+        public double[] notas { get; set; }
+
+        public double media { get; set; }
+
+    }
+}
---
+++ TesteEmpresa/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TesteEmpresa;
+
+Console.WriteLine("Hello, World!");
+
+
+var alunos = descerializar.executar();
+
+for (int i = 0; i < alunos.Length; i++)
+{
+    var alun = alunos[i];
+    var quantNota = alun.notas.Length;
+    var soma = alun.notas.Sum();
+
+    var media = soma / quantNota;
+    alun.media = media;
+    //Console.WriteLine("A média do aluno {0} é {1} ", alun.nome, media);
+}
+
+var mediaGeral = alunos.Sum(x => x.media) / alunos.Length;
+
+Console.WriteLine("A meidia geral é {0}. ", mediaGeral.ToString("N2"));
+
+foreach (var alun in alunos.OrderByDescending(x=> x.media)) {
+
+    Console.WriteLine(" {0}: {1} ",alun.nome,alun.media.ToString("N2"));
+}
+
+
+
+
+
+
+
